08522bce21e307569bda2301e7a0c76a5d59383f
Clara Cassel <clara.tiare@live.fr>
2018-03-21 13:53:37 +0100

    bon il y a encore des erreurs je sais pas pq

--- codeigniter/application/views/page_home.php
+++ codeigniter/application/views/page_home.php
@@ -1,7 +1,6 @@
 <?php
-$this->load->view('create_post');
-$this->load->view('post_list');
-$this->load->view('user_list');
-
+    $this->load->view('create_post');
+    $this->load->view('post_list');
+    $this->load->view('user_list');
 ?>
 
